1d3b9c0837930e1c4854560f922aad6d2b650e9b
happyAnger6 <happyAnger6@163.com>
2017-09-12 08:38:16 +0800

    add src url

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ hbs.registerHelper('truncate', function(content){
 });
 
 var app = express();
-
+app.listen(80)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
